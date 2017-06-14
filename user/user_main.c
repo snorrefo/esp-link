c090e0ea6a6f31e4133fd17df1e20c3ac681eb02
Snorre Foss Westman <snorrefo@gmail.com>
2017-06-14 23:29:10 +0200

    MWE for starting matt and printing SFW:... to debug log

--- user/user_main.c
+++ user/user_main.c
@@ -1,7 +1,52 @@
 #include <esp8266.h>
 #include <config.h>
 
-// initialize the custom stuff that goes beyond esp-link
+#ifdef SYSLOG
+#include "syslog.h"
+#define NOTICE(format, ...) do {                                            \
+  LOG_NOTICE(format, ## __VA_ARGS__ );                                      \
+  os_printf(format "\n", ## __VA_ARGS__);                                   \
+} while ( 0 )
+#else
+#define NOTICE(format, ...) do {                                            \
+  os_printf(format "\n", ## __VA_ARGS__);                                   \
+} while ( 0 )
+#endif
+
+// static char json_ble_send[500];
+static os_timer_t mqtt_timer;
+
+// #include "stdout.h"
+#include "mqtt.h"
+#include "mqtt_client.h"
+extern MQTT_Client mqttClient;
+
+
+extern void mqtt_client_init(void);
+static void ICACHE_FLASH_ATTR mqtt_check(void *args)
+{
+
+  if (mqttClient.connState != MQTT_CONNECTED)
+	{
+		NOTICE("====================== MQTT BAD, restarting\r\n");
+		//force a watchdog timeout and reset by doing something stupid like this here loop
+		while(1){};
+	}
+}
+
+
 void app_init() {
 
+
+  // stdoutInit();
+
+
+
+  NOTICE("SFW: initializing MQTT");
+  mqtt_client_init();
+
+  //MQTT Timer
+	os_timer_disarm(&mqtt_timer);
+	os_timer_setfn(&mqtt_timer, (os_timer_func_t *)mqtt_check, NULL);
+	os_timer_arm(&mqtt_timer, 60000, 1);
 }
